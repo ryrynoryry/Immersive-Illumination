f9898e624e7b2caa19cfa1e6203df123963c387b
Ryon Owings <reorh6@mst.edu>
2021-12-19 20:47:45 -0600

    Initial implementation of color selection

---
+++ ProjectDemo/lights.js
@@ -0,0 +1,29 @@
+//JS file to control the setting of the lights and select colors
+
+const lightsSection = document.querySelector("#lightsSection");
+const colorDiv = lightsSection.querySelector(".color");
+const selector = colorDiv.querySelector(".selector");
+let circle = colorDiv.querySelector(".circle");
+let square = colorDiv.querySelector(".square");
+selector.oninput = function() {
+  circle.style.backgroundColor = selector.value;
+}
+
+selector.onchange = function() {
+  square.style.backgroundColor = selector.value;
+  square.innerHTML = selector.value;
+  // TODO: Write to file
+  //LightsWriteValue(selector.value, "writeValue.php", "color.txt", "w+");
+}
+
+function LightsWriteValue(text, phpFile, fileName, mode) {
+  $.ajax({
+    type: 'POST',
+    url: phpFile,
+    data: {value: text, fileName: fileName, fileMode: mode},
+    // key value pair created, 'something' is the key, 'foo' is the value
+    success: function(result) {
+      console.log( '"' + text + '" was successfully sent to the server');
+    }
+  });
+};
