11d39109d8d5c4fb4b01cdcb06be1d91f50ec545
ryrynoryry <ryrynoryry@gmail.com>
2022-04-23 21:41:33 -0500

    Updated LavaLamp to use precomputed noise images. Updated Noise.py to generate variants of images.

--- ProjectDemo/scripts/Animations/LavaLamp.py
+++ ProjectDemo/scripts/Animations/LavaLamp.py
@@ -1,5 +1,7 @@
 from .BaseAnimation import BaseAnimation
-from Noise import fBmNoise
+# from Noise import fBmNoise
+from PIL import Image
+from os.path import exists
 
 class LavaLamp(BaseAnimation):
   def __init__(self, layer, args):
@@ -8,27 +10,30 @@ class LavaLamp(BaseAnimation):
     self.looping = True
     self.color1 = self.ToRGB(args[0]["value"])
     self.color2 = self.ToRGB(args[1]["value"])
-    self.speed = abs(float(args[2]["value"]))
+    self.speed = float(args[2]["value"])/25
 
     self.iteration = 0
     self.row = 0
     self.nextRow = 0
     self.transition = 0
-    self.size = 512
+    self.finalRow = 2048
     self.noisePic = None
+    self.needNewPic = True
 
     self.localStrip = [[self.color1[0], self.color1[1], self.color1[2]]] * self.NUM_PIXELS
 
   def Step(self):
     self.row = int(self.iteration)
-    self.nextRow = (self.row + 1) % self.size
-    self.transition = self.iteration - self.row
-    for i in range(self.NUM_PIXELS):
-      pixelColor = self.Lerp(
-          self.color1, self.color2, self.noisePic[self.row][i])
-      nextPixelColor = self.Lerp(
-          self.color1, self.color2, self.noisePic[self.nextRow][i])
-      self.localStrip[i] = [int(x + 0.5) for x in self.Lerp(pixelColor, nextPixelColor, self.transition)]
+    if self.speed >= 1:
+      for i in range(self.NUM_PIXELS):
+        self.localStrip[i] = [int(x + 0.5) for x in self.Lerp(self.color1, self.color2, self.noisePic[self.row][i]/128)]
+    else:
+      self.nextRow = (self.row + 1) % self.finalRow
+      self.transition = self.iteration - self.row # Fractional value between the two rows
+      for i in range(self.NUM_PIXELS):
+        pixelColor = self.Lerp(self.color1, self.color2, self.noisePic[self.row][i]/128)
+        nextPixelColor = self.Lerp(self.color1, self.color2, self.noisePic[self.nextRow][i]/128)
+        self.localStrip[i] = [int(x + 0.5) for x in self.Lerp(pixelColor, nextPixelColor, self.transition)]
 
     self.AquireLock()
     for i in range(self.NUM_PIXELS):
@@ -38,22 +43,34 @@ class LavaLamp(BaseAnimation):
     # Number of rows between each frame.
     # If the speed is < 1, linear interpolation will be used between rows.
     self.iteration += self.speed
-    if self.iteration >= self.size:
-      self.iteration = self.iteration - int(self.iteration)
+    if self.iteration >= self.finalRow:
+      self.iteration = self.iteration - self.finalRow
 
     return True
 
   def Setup(self, args):
-    freq, octs, noise = 1/64.0, 1, []
-    for y in range(self.size):
-      for x in range(self.size):
-        # Execute noise function to add each pixel to a 1D list.
-        noise.append(fBmNoise(x*freq, y*freq, int(self.size*freq), octs))
-      print(f"Working on row: {y}")
+    self.color1 = self.ToRGB(args[0]["value"])
+    self.color2 = self.ToRGB(args[1]["value"])
+    self.speed = float(args[2]["value"])/25
 
-    # Normalize to [0,1] while converting to a 2D list of "size x size"
-    myMax = max(noise)
-    myMin = min(noise)
-    self.noisePic = [[(noise[j * self.size + i] - myMin) / (myMax - myMin)
-                      for i in range(self.size)] for j in range(self.size)]
+    if self.speed >= 1.0:
+      self.iteration = self.iteration - int(self.iteration)
+
+    imageIndex = 0
+    octaves = 1
+    period = 64
+    if self.needNewPic:
+      self.noisePic = self.GetNoiseFile(imageIndex, octaves, period)
+      self.needNewPic = False
+
+  def GetNoiseFile(self, imgIndex, octaves, period):
+    fileName = "noise/noise%03d_%01d_%03d.png" % (imgIndex, octaves, period)
+    if (not exists(fileName)):
+      fileName = "noise/noise%03d_%01d_%03d.png" % (0, octaves, period)
+
+    # https://stackoverflow.com/a/41925811
+    im = Image.open(fileName)
+    data = list(im.getdata())
+    data2D = [data[i:(i+im.width)] for i in range(0, len(data), im.width)]
+    return data2D
 
--- ProjectDemo/scripts/Noise.py
+++ ProjectDemo/scripts/Noise.py
@@ -6,32 +6,113 @@
 
 import random
 import math
+from PIL import Image
+from os.path import exists
 
-period = 256
-perm = list(range(period))
-random.shuffle(perm)
-perm += perm
-dirs = [(math.cos(a * 2.0 * math.pi / period),
-         math.sin(a * 2.0 * math.pi / period))
-        for a in range(period)]
+import matplotlib.pyplot as plt
 
-def noise(x, y, per):
+# If iterations == 1, it will return the noise image
+def GenerateNoiseImage(iterations):
 
-  def surflet(gridX, gridY):
-    distX, distY = abs(x-gridX), abs(y-gridY)
-    polyX = 1 - 6*distX**5 + 15*distX**4 - 10*distX**3
-    polyY = 1 - 6*distY**5 + 15*distY**4 - 10*distY**3
-    hashed = perm[perm[int(gridX) % per] + int(gridY) % per]
-    grad = (x-gridX)*dirs[hashed][0] + (y-gridY)*dirs[hashed][1]
-    return polyX * polyY * grad
+  # Declare variables
+  period = 512
+  ySizeMultiple = 4
+  perm = list(range(period))
+  # random.seed(0)
+  random.shuffle(perm)
+  perm += perm
+  dirs = [(math.cos(a * 2.0 * math.pi / period),
+          math.sin(a * 2.0 * math.pi / period))
+          for a in range(period)]
 
-  intX, intY = int(x), int(y)
-  return (surflet(intX+0, intY+0) + surflet(intX+1, intY+0) +
-          surflet(intX+0, intY+1) + surflet(intX+1, intY+1))
+  # Define inner functions
+  def noise(x, y, per):
 
+    def surflet(gridX, gridY):
+      distX, distY = abs(x-gridX), abs(y-gridY)
+      polyX = 1 - 6*distX**5 + 15*distX**4 - 10*distX**3
+      polyY = 1 - 6*distY**5 + 15*distY**4 - 10*distY**3
+      hashed = perm[min(perm[min(int(gridX) % per, len(perm) - 1)] + int(gridY) % (per * ySizeMultiple), len(perm) - 1)]
+      grad = (x-gridX)*dirs[hashed][0] + (y-gridY)*dirs[hashed][1]
+      return polyX * polyY * grad
 
-def fBmNoise(x, y, per, octs):
-  val = 0
-  for o in range(octs):
-    val += 0.5**o * noise(x*2**o, y*2**o, per*2**o)
-  return val
+    intX, intY = int(x), int(y)
+    return (surflet(intX+0, intY+0) + surflet(intX+1, intY+0) +
+            surflet(intX+0, intY+1) + surflet(intX+1, intY+1))
+
+  def fBm(x, y, per, octs):
+    val = 0
+    for o in range(octs):
+      val += 0.5**o * noise(x*2**o, y*2**o, per*2**o)
+    return val
+
+  # Perform actions to Generate Noise Image
+  for saveCount in range(iterations):
+    perm = list(range(period))
+    # random.seed(0)
+    random.shuffle(perm)
+    perm += perm
+    dirs = [(math.cos(a * 2.0 * math.pi / period),
+            math.sin(a * 2.0 * math.pi / period))
+            for a in range(period)]
+
+    size, data = period, []
+    factors = [n for n in range(3, size + 1) if size % n == 0] # Search for factors greater than 2
+    frequencies = [1/float(f) for f in factors]
+    maxOctaves = 5
+    for octaves in range(1, maxOctaves):
+      for freq in frequencies:
+        data = []
+        for y in range(size * ySizeMultiple):
+            for x in range(size):
+                data.append(fBm(x*freq, y*freq, int(size*freq), octaves))
+
+        # Save data as an image and save in current working directory
+        im = Image.new("L", (size, size * ySizeMultiple))
+        im.putdata(data, 128, 128)
+
+        fileCount = 0
+        while exists("noise/noise%03d_%01d_%03d.png" % (fileCount, octaves, int(1/freq))):
+            fileCount += 1
+        fileName = "noise/noise%03d_%01d_%03d.png" % (fileCount, octaves, int(1/freq))
+        im.save(fileName)
+        print(f"Saved image for variant: {saveCount} (octaves: {octaves}, frequency {int(1/freq)})")
+
+        # Evaluate the image to get average pixel values.
+        # myMax = 0
+        # myMin = 1
+        # mySum = 0
+
+        # myMax = max(data)
+        # myMin = min(data)
+        # myAvg = sum(data) / len(data)
+
+        # print(f"Prelim Max: {myMax}")
+        # print(f"Prelim Min: {myMin}")
+        # print(f"Prelim Avg: {myAvg}")
+        # pic = [[(data[j * size + i] - myMin) / (myMax - myMin) for i in range(size)] for j in range(size)]
+
+        # mySum = 0
+        # myMax = 0
+        # myMin = 1
+        # for y in range(size):
+        #   for x in range(size):
+        #     mySum += pic[y][x]
+        #     myMax = max(myMax, pic[y][x])
+        #     myMin = min(myMin, pic[y][x])
+        # avg = mySum / (size * size)
+        # print(f"Final Max: {myMax}")
+        # print(f"Final Min: {myMin}")
+        # print(f"Final Avg: {avg}")
+
+        # Show matplotlib plot window
+        # plt.imshow(pic, cmap='gray')
+        # plt.show()
+
+        if iterations == 1:
+          data = list(im.getdata())
+          data2D = [data[i:(i+im.width)] for i in range(0, len(data), im.width)]
+          return data2D
+
+if __name__ == "__main__":
+  GenerateNoiseImage(10)
